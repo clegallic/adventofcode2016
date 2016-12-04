490c1dc2a2b2ae647d9a69a731201b053cda6ff2
Charles Le gallic <charles@amoae.com>
2016-12-04 19:13:02 +0100

    day one - part two

--- d01/d01.js
+++ d01/d01.js
@@ -5,9 +5,15 @@ var instructions = ['L4','R2','R4','L5','L3','L1','R4','R5','R1','R3','L3','L2',
 var directions = [[0,1],[1,0],[0,-1],[-1,0]];
 
 // Initial position and direction
-var x = y = 0;
+var x = y = previousx = previousy = 0;
 var heading = 0; // Heading North
 
+// Manage path
+var visitedLocations = [['visited']]; // First location is visited
+var firstLocationVisitedTwiceFound = false;
+var index = 1;
+
+// loop on array values
 instructions.map(current => {
   var offset = parseInt(current.substring(1, current.length)); // distance to move
   heading += current[0] == 'R' ? 1 : -1; // new heading after turning left or right
@@ -15,6 +21,27 @@ instructions.map(current => {
   if(heading > 3) heading = 0; // fix invalid headings
   x += directions[heading][0] * offset; // moving on X
   y += directions[heading][1] * offset; // moving on Y
+
+  // Searching for first location visited twice
+  if(!firstLocationVisitedTwiceFound){ // still haven't found what I'm looking for...
+    for(i = (previousx < x ? previousx + 1: x); i <= (previousx <= x ? x : previousx - 1); i++){
+      for(j = (previousy < y ? previousy + 1: y); j <= (previousy <= y ? y : previousy - 1); j++){
+        if(visitedLocations[i] == undefined) visitedLocations[i] = [];
+        if(visitedLocations[i][j] == undefined){
+          visitedLocations[i][j] = 'visited'; // mark location as visited
+        }
+        else{
+          console.log("First location visited twice : x = %s, y = %s, distance = %s", i, j, Math.abs(i) + Math.abs(j));
+          firstLocationVisitedTwiceFound = true;
+          break;
+        }
+      }
+    }
+    // Save previous location for path finding
+    previousx = x;
+    previousy = y;
+  }
+  index++;
 });
 
-console.log("x = %s, y = %s, distance = %s", x, y, Math.abs(x) + Math.abs(y));
+console.log("Final location : x = %s, y = %s, distance = %s", x, y, Math.abs(x) + Math.abs(y));
